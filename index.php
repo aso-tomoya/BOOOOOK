63d11edc5bafc827646da58dd165c18b0babcce6
aso-tomoya <2301380@s.asojuku.ac.jp>
2024-11-05 12:52:32 +0900

    o

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../script/style.css">
+    <link rel="stylesheet" href="./script/style.css">
     <title>トップ画面</title>
 </head>
 <body>
@@ -22,18 +22,19 @@
             <!-- 他の商品も同様に追加 -->
         </div>
     </div>
-
-    <div class="pickup">
-        <h2>ピックアップ</h2>
-        <div class="item-list">
-            <div class="item">
-                <img src="#" alt="商品画像">
-                <div class="title">タイトル</div>
-                <div class="price">¥0000</div>
+    <fieldset disabled="disabled">
+        <div class="pickup">
+            <h2>ピックアップ</h2>
+            <div class="item-list">
+                <div class="item">
+                    <img src="#" alt="商品画像">
+                    <div class="title">タイトル</div>
+                    <div class="price">¥0000</div>
+                </div>
+                <!-- 他の商品も同様に追加 -->
             </div>
-            <!-- 他の商品も同様に追加 -->
         </div>
-    </div>
+    </fieldset>
 </div>
 
 </body>
