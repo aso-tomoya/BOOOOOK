6555ff11f33950b1f6e42fc3e5340b62ca1a175b
aso-tomoya <2301380@s.asojuku.ac.jp>
2024-11-06 09:35:43 +0900

    a

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
         <input type="text" placeholder="検索">
         <button>🔍</button>
     </div>
+    <div class="cotainer">
     <button class="login-button">ログイン</button>
     <button class="cart-button">🛒</button>
+    </div>
 </header>
