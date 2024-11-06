9cd09be0c1ed30a85baae0a485e313c087957587
ryusei20040904 <2301344@s.asojuku.ac.jp>
2024-11-06 11:15:22 +0900

    a

--- search_results.php
+++ search_results.php
@@ -9,8 +9,31 @@
 <body>
 
 <?php include 'header.php' ?>
-
-
+<div class="container">
+    <h2>「〇〇〇」の検索結果</h2>
+    <p>〇〇件</p>
+    <div class="sort-options">
+        <button>人気順</button>
+    </div>
+    <div class="item-list">
+        <!-- Each item -->
+        <div class="item">
+            <img src="placeholder.png" alt="Book Cover">
+            <div class="title">タイトル</div>
+            <div class="author">作者</div>
+            <div class="price">価格 ￥〇〇</div>
+            <div class="genre">ジャンル</div>
+        </div>
+        <div class="item">
+            <img src="placeholder.png" alt="Book Cover">
+            <div class="title">タイトル</div>
+            <div class="author">作者</div>
+            <div class="price">価格 ￥〇〇</div>
+            <div class="genre">ジャンル</div>
+        </div>
+        <!-- Repeat items as needed -->
+    </div>
+</div>
 
 </body>
 </html>
