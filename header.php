569167102d75e6204147cef1e0aa5740737b685f
aso-tomoya <2301380@s.asojuku.ac.jp>
2024-11-07 09:42:07 +0900

    p

--- header.php
+++ header.php
@@ -1,5 +1,9 @@
 <header>
-    <h1>BOOOOOK</h1>
+
+    <!-- タイトルロゴ -->
+    <h1><a href="index.php" class="link"><img src="img/BOOOOOK.png" alt="BOOOOOK" width="200"></a></h1>
+
+    <!-- 検索バー -->
     <div class="search-bar">
         <input type="text" placeholder="検索">
         <button>🔍</button>
