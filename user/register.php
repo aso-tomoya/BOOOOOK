93d9b4b7443cf24752d4b2acf663fe6c04e83a31
mituyy <2301374@s.asojuku.ac.jp>
2024-11-19 10:45:18 +0900

    register

--- user/register.php
+++ user/register.php
@@ -16,7 +16,7 @@
             border: none;
         }
         .textbox{
-            width: 200px;
+            width: 350px;
             border-radius: 25px; /* 楕円形にする */
             padding: 4px 10px;
             border: 1.5px solid #000000;
@@ -32,41 +32,47 @@
 
 <?php include '../header.php' ?>
 
-<div style="border: solid 1px #000000; padding: 30px; border-width: 2px; border-radius: 10px; margin: 10px 400px;">
+<div style="border: solid 1px #000000; padding: 20px 30px; border-width: 2px; border-radius: 10px; margin: 10px 400px;">
 
 <div class="register-container">
     <h2 class="register-title" style="text-align:center">アカウントを作成</h2>
-    <p></p>
+    <p>*は必須項目</p>
     <form action="register_execution.php" method="post" class="register-form">
         <div class="form-group">
-            <p style="text-align:center"><label for="name">氏名*</label><br>
+            <label for="name">氏名*</label><br>
             <p style="text-align:center"><input type="text" name="name" id="name" class="textbox" required><br>
         </div>
         <div class="form-group">
-            <p style="text-align:center"><label for="mail">メールアドレス*</label><br>
+            <label for="mail">メールアドレス*</label><br>
             <p style="text-align:center"><input type="text" name="mail" id="mail" class="textbox" required><br>
         </div>
         <div class="form-group">
-            <p style="text-align:center"><label for="password1" >パスワード*</label><br>
-            <p style="text-align:center"><input type="password" name="password1" id="password1" class="textbox" required><br><br>
+            <label for="password1" >パスワード*</label><br>
+            <p style="text-align:center"><input type="password" name="password1" id="password1" class="textbox" required><br>
         </div>
         <div class="form-group">
-            <p style="text-align:center"><label for="password2">パスワードを確認*</label><br>
+            <label for="password2">パスワードを確認*</label><br>
+            <?php
+            if(isset($_GET['error1'])){
+                echo '<p style="color:red" style="text-align:center">パスワードが異なっています</p>';
+            }
+        ?>
             <p style="text-align:center"><input type="password" name="password2" id="password2" class="textbox" required><br>
         </div>
         <div class="form-group">
-            <p style="text-align:center"><label for="postcord">郵便番号*</label><br>
-            <p style="text-align:center"><input type="text" name="postcord" id="postcord" class="postcord" required><br>
+            <p><label for="postcord">郵便番号*</label></p>
+            <p><input type="text" name="postcord" id="postcord" class="postcord" required></p>
         </div>
         <div class="form-group">
-            <p style="text-align:center"><label for="address">住所(建物名も含む)*</label><br>
-            <p style="text-align:center"><input type="text" name="address" id="address" class="textbox" required><br>
+            <label for="address">住所(建物名も含む)*</label><br>
+            <p style="text-align:center"><input type="text" name="address" id="address" class="textbox" required><br><br>
         </div>
 
+        
         <?php
-            if(isset($_GET['error'])){
-                echo '<p style="color:red" style="text-align:center">メールアドレスかパスワードが正しくありません。</p>';
-            }    
+            if(isset($_GET['error2'])){
+                echo '<p style="color:red" style="text-align:center">未入力の項目があります</p>';
+            }
         ?>
 
         <p style="text-align:center"><input type="submit" class="button" name="action" value="アカウント作成">
