a8890e9387d312d1b1199e11c2a5779d6459a885
mituyy <2301374@s.asojuku.ac.jp>
2024-11-25 12:25:17 +0900

    indexbutton

--- admin/index.php
+++ admin/index.php
@@ -9,12 +9,15 @@
 <body>
 
 <?php include '../header.php' ?>
-<form action="manage_products.php">
-    <input type="submit" name="pro_ad" value="商品管理">
-</form>
-<form action="manage_users.php">
-    <input type="submit" name="user_ad" value="ユーザー管理">
-</form>
+<div class="container">
+    <form action="manage_products.php">
+        <div class="sel_botton"><input type="submit" name="pro_ad" value="商品管理"></div>
+    </form>
+
+    <form action="manage_users.php">
+        <div class="sel_botton"><input type="submit" name="user_ad" value="ユーザー管理"></div>
+    </form>
+</div>
 
 </body>
 </html>
