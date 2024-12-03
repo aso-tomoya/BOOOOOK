33cf1a196a26be765cf6df53d0e2fcd645719986
mituyy <2301374@s.asojuku.ac.jp>
2024-12-03 12:44:44 +0900

    a

--- admin/add_product.php
+++ admin/add_product.php
@@ -7,10 +7,22 @@
     <title>商品追加</title>
 </head>
 <body>
-
+<form action=".php" method="post" enctype="multipart/form-data">
 <?php include_once '../header.php' ?>
-
-
+商品の追加
+タイトル
+<input type="text" name="title">
+ジャンル
+<input type="text" name="genre">
+作者名
+<input type="text" name="author">
+金額
+<input type="text" name="item_price">
+発売日
+<input type="text" name="relaese_date">
+説明
+<textarea name="summary" id="summary" rows="4"></textarea>
+<input type="file" name="file">
 
 </body>
 </html>
