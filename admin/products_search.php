53cdbe425e9ca2e46574b9b7238762c48e1724b2
mituyy <2301374@s.asojuku.ac.jp>
2024-12-12 10:11:03 +0900

    Update products_search.php

--- admin/products_search.php
+++ admin/products_search.php
@@ -11,29 +11,33 @@ $pdo = new PDO('mysql:host=mysql311.phy.lolipop.lan;
         dbname=LAA1557203-boooook;charset=utf8',
         'LAA1557203',
         'boooook');
+?>
+        <div class="form-container">
 
-        if(isset($_POST['word'])){
-            $search_term = '%' . $_POST['word'] . '%';
+    <?php if(isset($_POST['word'])){
+        $search_term = '%' . $_POST['word'] . '%';
             
         $sql='select * from item inner join genre ON item.genre_id = genre.genre_id where item.item_name LIKE ? or item.author LIKE ? or item.summary LIKE ?  or genre.genre_name LIKE ?';
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$search_term,$search_term,$search_term,$search_term]);
 
-        foreach($pdo->query('select * from item') as $row){
-            echo '<div class="pro-info">';
-            echo '<div style="-moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; border: #a9a9a9 solid 1px; font-size: 100%; padding: 20px;width: 320px;height: 150px;">';
-            echo '<form="product_delete.php" method="post">';
-            echo '<img src="../img/',$row['item_img'],'">';
-            echo $row['item_name'],'<br>';
-            echo $row['author'],'<br>';
-            echo $row['item_price'],'円<br>';
-            echo '<p style="text-align:right"><input type="submit" value="商品削除">';
-            echo '</div>';
-            echo '<br>';
-            echo '</div>';
-        }
-    }
-        $pdo = null;
-        ?>
+        foreach($stmt as $row):?>
+            <div class="pro-info" style="width: 100%;">
+                <div class="item-box">
+                    <form="product_delete.php" method="post">
+                        <img src="../img/<?= $row['item_img'] ?>" alt="<?= $row['item_name'] ?>" style="width: 100px;">
+                        <p><?= $row['item_name'] ?><br>
+                        <?= $row['author'] ?><br>
+                        <?= $row['item_price'] ?>円</p>
+                        <p style="text-align:right">
+                            <input type="hidden" name="item_id" value="<?= $row['item_id'] ?>">
+                            <input type="submit" value="商品削除">
+                        </p>
+                    </form>
+                </div>
+            </div>
+        <?php endforeach; 
+    } ?>
+    </div>
 </body>
 </html>
